b829a99d70fafe8fbca06a52c9c000b7ce961a74
bozhko-victor <bozhko.v.n@yandex.ru>
2021-09-27 13:29:21 +0300

    Modified build.gradle

--- src/test/java/guru/qa/ParametrazedTest.java
+++ src/test/java/guru/qa/ParametrazedTest.java
@@ -17,17 +17,8 @@ import static com.codeborne.selenide.Selenide.actions;
 import static com.codeborne.selenide.impl.Cleanup.of;
 import static org.openqa.selenium.By.linkText;
 
-public class ParametrazedTest {
+public class ParametrazedTest extends TestBase {
 
-    static final String URL1 = "https://asana.com/ru/create-account";
-    static final String URL2 = "https://englishtochka.ru/webinar/";
-    static final String URL3 = "https://lookfreedom.ru/napisat-nam/";
-
-    @BeforeAll
-    static void setup() {
-        Configuration.browser = "chrome";
-        Configuration.startMaximized = true;
-    }
 
     @ValueSource(strings = {
             "sergion@fifi.ru",
